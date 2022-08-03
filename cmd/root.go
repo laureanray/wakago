0a8a5d9dcc5cb2aef9135ed3a5e7209c999d92b5
laureanray <laureanraybahala@gmail.com>
2022-08-03 19:44:43 +0800

    updated command descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "wakago",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "wakago is a terminal client for wakatime using its official v1 api",
+	Long: `wakago is a terminal client for wakatime using its official v1 api
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  primary use case is to use this for ricing your Linux setup with status bars like polybar and xmobar
+  `,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +43,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
